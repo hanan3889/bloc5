c461d982f840c5390f38134b2d23862649e99568
hanan3889 <rebaia_hanan@yahoo.fr>
2025-07-02 09:17:33 +0200

    refacto code

--- start-dev.sh
+++ start-dev.sh
@@ -4,4 +4,4 @@ echo "🚀 Démarrage de l'environnement DEV..."
 
 docker-compose -f docker-compose.dev.yaml -p vide-grenier-dev up -d web-dev db-dev
 
-echo "🎉 Environnement DEV démarré sur http://videgrenier-dev:8000"
+echo "🎉 Environnement DEV démarré sur http://localhost:8000"
